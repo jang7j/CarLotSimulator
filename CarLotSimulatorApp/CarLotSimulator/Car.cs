60ad86d132ec960281031be6de0bd872e493b72f
Yun Jang <jang7j@yahoo.com>
2022-08-18 15:13:09 -0500

    initial commit

---
+++ CarLotSimulatorApp/CarLotSimulator/Car.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarLotSimulator
+{
+    public Car() //default constructor
+    
+    internal class Car(string make, string model, int year) //parameterized constructor
+    {
+        Make = make;
+        Model = model;
+        Year = year;
+    }
+        public int Year { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string EngineNoise { get; set; }
+        public string HonkNoise { get; set; }
+        public int isDriveable { get; set; }
+
+        public void MakeEngineNoise(string noise)
+        {
+            Console.WriteLine($"My car's engine sounds like {noise}");
+        }
+        public void MakeHonkNoise()
+        {
+            Console.WriteLine($"My car's honk sounds like {HonkNoise}");
+        }
+
+
+    }
+
+}
--- CarLotSimulatorApp/CarLotSimulator/Program.cs
+++ CarLotSimulatorApp/CarLotSimulator/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 
 namespace CarLotSimulator
 {
@@ -6,6 +7,7 @@ namespace CarLotSimulator
     {
         static void Main(string[] args)
         {
+            CarLot carlot = new CarLot();
             //TODO
 
             //Create a seperate class file called Car
@@ -18,6 +20,37 @@ namespace CarLotSimulator
             //Set the properties for each of the cars
             //Call each of the methods for each car
 
+            var instance1 = new Car(); //same as Car instance1 = new Car();
+            var instance2 = new Car();
+            var instance3 = new Car();
+           
+            //Set Properties by:
+            //1) Dot notation
+            instance1.Make = "Honda"; 
+            instance1.Model = "Accord";
+            instance1.Year = 2009; "
+            instance1.EngineNoise = true;
+            instance1.HonkNoise = true;
+            instance1.isDriveable = true;
+
+            //name of collection, dot add, pass in the car
+            carlot.CarList.Add(instance1);
+            instance1.Make
+                 
+            //2) Object initializer syntax
+            var instance2 = new Car() { 
+                Make = "VW", 
+                Model = "Beetle", 
+                EngineNoise = "Beep", 
+                HonkNoise = "Honk", 
+                isDriveable = true };
+            //var instance2 = new Car() { Make = "VW", Model = "Beetle", EngineNoise = "Beep", HonkNoise = "Honk", isDriveable = true };
+            carlot.CarList.Add(instance2);
+
+            //3) Custom constructor - go back to the class Car & set the parameters & asign properties with those parameters:Make = make; Model = model; Year = year;
+            var instance3 = new Car("Jeep", "Wrangler", 2022);
+            carlot.CarList.Add(instance3);
+
             //*************BONUS*************//
 
             // Set the properties utilizing the 3 different ways we learned about, one way for each car
@@ -28,6 +61,14 @@ namespace CarLotSimulator
             //It should have at least one property: a List of cars
             //Instanciate the a Carlot at the beginning of the program and as you create a car add the car to the list.
             //At the end iterate through the list printing each of car's Year, Make, and Model to the console
+            foreach (var item in carlot.CarList)
+            {
+                Console.WriteLine(item.Year);
+                Console.WriteLine(item.Make);
+                Console.WriteLine(item.Model);
+                item.MakeEngineNoise("Vroom");
+                item.MakeHonkNoise(item.HonkNoise);
+            }
         }
     }
 }
